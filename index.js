d8c8893f0aa6594a747977e2e19c1d8412dff0f7
gege83var <56068139+gege83var@users.noreply.github.com>
2021-09-15 17:58:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ client.on('message', message => {
 
             var hasNonAdminRole = true
             for(let i = 0; i<process.env.adminsID.length; i++){
-		    console.log(process.env.adminsID);
+		    console.log(message.member.roles.cache);
+		    console.log(message.member.roles.cache.has(process.env.adminsID[i]));
                 if(message.member.roles.cache.has(process.env.adminsID[i])){
                     hasNonAdminRole = false
                     break
